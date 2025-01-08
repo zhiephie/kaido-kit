67cb895989893009d93b2cb4c22fb75ef05ba869
siubie <putraprima@gmail.com>
2025-01-08 23:36:40 +0800

    Configure Filament admin panel path and add initial Filament configuration file; comment out default route

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('')
             ->login(Login::class)
             // ->login()
             ->colors([

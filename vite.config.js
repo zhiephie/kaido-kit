b745e4140092b99bcbbc480b1b3fb9e2b5238f52
siubie <putraprima@gmail.com>
2025-01-17 05:49:51 +0700

    Update Vite configuration to include app/Providers for live reload

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: [...refreshPaths, "app/Livewire/**", "app/Filament/**"],
+            refresh: [
+                ...refreshPaths,
+                "app/Livewire/**",
+                "app/Filament/**",
+                "app/Providers/**",
+            ],
         }),
     ],
 });
